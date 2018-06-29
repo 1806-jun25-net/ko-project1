a462d3a905ef925550720b102bd0bdd2b3bd632a
kevinhko <ko.kevin96@gmail.com>
2018-06-29 16:43:26 -0400

    Initial files

---
+++ PizzaStore/PizzaStore.Library/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaStore.Library
+{
+    public class Address
+    {
+        public string Street;
+        public string City;
+        public string State;
+        public string ZipCode;
+    }
+}
---
+++ PizzaStore/PizzaStore.Library/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaStore.Library
+{
+    class User
+    {
+        public string FirstName;
+        public string LastName;
+        public Address defaultLocation;
+        public 
+    }
+}
