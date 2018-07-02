f890073549db2b4013c49712ebf4fc8c6229740d
kevinhko <ko.kevin96@gmail.com>
2018-07-02 16:49:22 -0400

    have base classes. Did the console update and created database

---
+++ PizzaStore/PizzaStore.Library/Pizza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaStore.Library
+{
+    class Pizza
+    {
+        public string CrustStyle { get; set; }
+        public string PizzaSize { get; set; }
+        public string Sauce { get; set; }
+        public string Cheese { get; set; }
+        public string[] Toppings { get; set; }
+    }
+}
